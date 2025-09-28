9d072e5537c1044effe292a369faaf4bb4357021
Luan Alves <115920169+akanaul@users.noreply.github.com>
2025-09-28 03:51:45 -0300

    Resolução do primeiro algoritmo

---
+++ algoritmos/cocat_dados.py
@@ -0,0 +1,8 @@
+# Receber dois dados diferentes do user e concatenar os valores em uma string
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+resultado = info1 + " " + info2
+
+print("As informações concatenadas são:", resultado)
